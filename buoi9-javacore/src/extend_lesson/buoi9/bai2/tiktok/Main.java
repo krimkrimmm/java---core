1936e2ca2888ec014595b5458c8ccef95c36b160
krimkrimmm <147626046+krimkrimmm@users.noreply.github.com>
2024-08-29 19:18:39 +0700

    Update Main.java

--- buoi9-javacore/src/extend_lesson/buoi9/bai2/tiktok/Main.java
+++ buoi9-javacore/src/extend_lesson/buoi9/bai2/tiktok/Main.java
@@ -13,38 +13,38 @@ import extend_lesson.buoi9.bai2.tiktok.entities.Tiktok;
 public class Main {
     public static void main(String[] args) {
         // Create an idol
-//        Idol idol1 = new Idol("a", 1, "aboy.com", "booy");
-//
-//        idol1.followers.add(new Follower("b", 101, "bgmail.com", 500));
-//        idol1.followers.add(new Follower("c", 102, "cyahoo.com", 600));
-//
+       Idol idol1 = new Idol("a", 1, "aboy.com", "booy");
+
+       idol1.followers.add(new Follower("b", 101, "bgmail.com", 500));
+        idol1.followers.add(new Follower("c", 102, "cyahoo.com", 600));
+
 //        // Create a song
-//        Song song1 = new Song(1001, "How You Like That", "booy@gmail.com");
-//        // Display idol info
-//        displayIdolInfo(idol1);
-//        // Display song info
-//        displaySongInfo(song1);
-//    }
-//    public static void displayIdolInfo(Idol idol) {
-//        System.out.println("Idol Information:");
-//        System.out.println("Name: " + idol.name);
-//        System.out.println("ID: " + idol.id);
-//        System.out.println("Email: " + idol.email);
-//        System.out.println("Group: " + idol.group);
-//        System.out.println("Followers:");
-//        for (Follower follower : idol.followers) {
-//            System.out.println("  - Name: " + follower.name);
-//            System.out.println("    ID: " + follower.id);
-//            System.out.println("    Email: " + follower.email);
-//            System.out.println("    Number of Likes: " + follower.NumberOfLike);
-//        }
-//    }
-//    public static void displaySongInfo(Song song) {
-//        System.out.println("\nSong Information:");
-//        System.out.println("ID: " + song.id);
-//        System.out.println("Name: " + song.name);
-//        System.out.println("Singer: " + song.singer);
-//    }
-//
-//
+        Song song1 = new Song(1001, "How You Like That", "booy@gmail.com");
+      // Display idol info
+        displayIdolInfo(idol1);
+       // Display song info
+        displaySongInfo(song1);
+    }
+    public static void displayIdolInfo(Idol idol) {
+        System.out.println("Idol Information:");
+        System.out.println("Name: " + idol.name);
+        System.out.println("ID: " + idol.id);
+       System.out.println("Email: " + idol.email);
+        System.out.println("Group: " + idol.group);
+       System.out.println("Followers:");
+       for (Follower follower : idol.followers) {
+          System.out.println("  - Name: " + follower.name);
+            System.out.println("    ID: " + follower.id);
+          System.out.println("    Email: " + follower.email);
+            System.out.println("    Number of Likes: " + follower.NumberOfLike);
+        }
+    }
+    public static void displaySongInfo(Song song) {
+       System.out.println("\nSong Information:");
+       System.out.println("ID: " + song.id);
+        System.out.println("Name: " + song.name);
+        System.out.println("Singer: " + song.singer);
+    }
+
+
     }
